1c24c227abcf64ad3e9c2cdbebbf7f21f42c3f3c
sarujan1 <sarujan@codelantic.com>
2025-04-18 15:28:59 +0530

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,49 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ScrollView, StatusBar, Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/searchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
+  const [searchList, setSearchList] = useState("");
   return (
     <View
-      className="flex-1 items-center justify-center bg-red-500"
+      className="flex-1 bg-primary"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      {/* <Link href="/onboarding">Go to onboarding</Link>
-      <Link href="/movieDetails/1">Go to movie details</Link> */}
+      {/* Transparent status bar for Android */}
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      {/* Background image */}
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+      />
+      {/* ScrollView */}
+      <ScrollView
+        className="flex-1 px-5" 
+      >
+          <Image
+            source={icons.logo} 
+            className=" w-12 z-10 mt-20 mb-5 mx-auto"
+          />
+
+          {/* Search bar */}
+          <View className="flex-1 mt-5">
+            <SearchBar
+              placeholder="Search for a movie"
+              value={searchList}
+              onChangeText={setSearchList}
+              // InLine function
+              onPress={() => router.push(`/search`)}
+            />
+          </View>
+      </ScrollView>  
     </View>
   );
 }
---
+++ src/components/searchBar.tsx
@@ -0,0 +1,54 @@
+import { Image, StyleSheet, TextInput, View } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+interface SearchBarProps {
+  placeholder: string
+  value: string
+  onChangeText: (text: string) => void
+  onPress: () => void
+}
+
+const SearchBar = (props: SearchBarProps) => {
+  const { placeholder, value, onChangeText, onPress } = props
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={icons.search}
+        style={styles.icon}
+        resizeMode="contain"
+      />
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor={'#a8b5db'}
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeText}
+        onPressIn={onPress}
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#0f0d23', // your dark-200
+    marginHorizontal: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    tintColor: '#ab8bff',
+    marginRight: 10, // this is the gap!
+  },
+  input: {
+    flex: 1,
+    color: '#ffffff',
+    fontSize: 16,
+  },
+})
